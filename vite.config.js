1271747d0c06f4e6e63ebb047ce3f3ee08dcb8cb
Lenin Meza <e_ljmeza@bancoppel.com>
2025-04-09 16:06:20 -0600

    :bug: Fix: Environment vars

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import mkcert from 'vite-plugin-mkcert'
 
-export default defineConfig({
-  server: {
-    https: true
-  },
-  plugins: [mkcert()],
-  base: '/incode-face-login/',
-  envDir: 'environment',
-});
+export default defineConfig(({ mode }) => {
+  // Load the current env file (production in build case) located in the environment directory
+  const env = loadEnv(mode, 'environment');
+
+  return {
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
+      'import.meta.env.VITE_SDK_URL': 'https://sdk.incode.com/sdk/onBoarding-1.73.1.js'
+    },
+    server: {
+      https: true,
+    },
+    plugins: [mkcert()],
+    base: '/incode-face-login/',
+    envDir: 'environment',
+  }
+})
