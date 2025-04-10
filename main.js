367635622180bcc5f503ac6f2e4f227e246a8576
Lenin Meza <e_ljmeza@bancoppel.com>
2025-04-09 18:42:42 -0600

    :wrench: API Key added

--- main.js
+++ main.js
@@ -99,7 +99,7 @@ function finishOnboarding() {
 
 async function app() {
   console.log("VITE_API_URL", import.meta.env.VITE_API_URL);
-  console.log("VITE_API_KEY", import.meta.env.VITE_API_KEY);
+  console.log("VITE_API_KEY", import.meta.env.VITE_FAKE_BACKEND_APIURL);
   try {
     const apiURL = import.meta.env.VITE_API_URL;
     const apiKey = import.meta.env.VITE_FAKE_BACKEND_APIURL;
